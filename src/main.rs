497857e29c837375df1f9f64e2fc7f4b9e365621
pandorasNox <itspandorasnox@gmail.com>
2020-07-29 19:08:32 +0200

    init + firs isbn part

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let maybe_isbn = std::env::args().nth(1).expect("no pattern given");
+    let cleaned_isbn = maybe_isbn.replace("-", "");
+    // println!("{}", cleaned_isbn);
+    // 123456789-X
+
+    if cleaned_isbn.chars().count() != 10 {
+        panic!("isbn has to be 10 characters long, you currently got {}", cleaned_isbn.chars().count());
+    }
+
+    let maybe_last = cleaned_isbn.chars().last();
+    let last = match maybe_last {
+        Some(last) => last,
+        None => {panic!("this is a terrible mistake!");},
+    };
+    
+    if last.is_digit(10) == false && last != 'X' {
+        panic!("last letter \"{}\" is not a digit or \"X\"", last);
+    }
+
+    let cleaned_isbn = cleaned_isbn.to_string();
+    let sliced_isbn = &cleaned_isbn[..cleaned_isbn.len()-1];
+    println!("sliced_isbn: {}", sliced_isbn);
+}
