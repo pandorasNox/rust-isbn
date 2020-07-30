207455a3db5768a03f868d3ddb53a2043a034ba6
Leonard Kriese <leonard.kriese@turbinekreuzberg.com>
2020-07-30 14:35:46 +0100

    finalize isbn parser and validator

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,30 @@ fn main() {
         None => {panic!("this is a terrible mistake!");},
     };
 
-    if last.is_digit(10) == false && last != 'X' {
-        panic!("last letter \"{}\" is not a digit or \"X\"", last);
-    }
+    let last_numeric = if last.is_digit(10) {last.to_digit(10)} else {Some(10)};
+    let last_numeric = match last_numeric {
+        Some(i) => i,
+        None => panic!("last letter \"{}\" is not a digit or \"X\"", last),
+    };
 
     let sliced_isbn = (&cleaned_isbn[..cleaned_isbn.len()-1]).to_string();
     println!("sliced_isbn: {}", sliced_isbn);
+
+    let mut result = 0;
+    let mut counter = 10;
+    for character in sliced_isbn.chars() {
+        result = match character.to_digit(10) {
+            Some(i) => result + counter * i,
+            None => panic!("letter \"{}\" is not a digit", character),
+        };
+        counter = counter - 1;
+    }
+
+    result = result + last_numeric;
+
+    if let 0 = result % 11 {
+       println!("{} is a valid ISBN", "maybe_isbn")
+    } else {
+        println!("{} is not a valid ISBN", "maybe_isbn")
+    }
 }
