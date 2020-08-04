5f5b41826da8319aaf25986026b9a74426f58b91
pandorasNox <itspandorasnox@gmail.com>
2020-08-04 18:40:29 +0200

    enum experiement

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
+
+enum Isbn {
+    Isbn10(String),
+    Error(String),
+}
+
+impl Isbn {
+    fn from_string(maybe_isbn:String) -> Isbn {
+        if maybe_isbn.chars().count() != 10 {
+            let err = format!("isbn has to be 10 characters long, you currently got {}", maybe_isbn.chars().count());
+            return Isbn::Error(err.to_string());
+        }
+
+        return Isbn::Isbn10(maybe_isbn)
+    }
+}
+
 fn main() {
     let maybe_isbn = std::env::args().nth(1).expect("no pattern given");
     let cleaned_isbn = maybe_isbn.replace("-", "");
+    let cleaned_isbn_cp = cleaned_isbn.clone();
     // println!("{}", cleaned_isbn);
     // 123456789-X
 
-    if cleaned_isbn.chars().count() != 10 {
-        panic!("isbn has to be 10 characters long, you currently got {}", cleaned_isbn.chars().count());
+    let sono_isbn = Isbn::from_string(cleaned_isbn);
+    match sono_isbn {
+        Isbn::Isbn10(isbn10) => println!("{}", isbn10),
+        Isbn::Error(err) => {panic!("{}", err)},
+    };
+
+    if cleaned_isbn_cp.chars().count() != 10 {
+        panic!("isbn has to be 10 characters long, you currently got {}", cleaned_isbn_cp.chars().count());
     }
 
-    let maybe_last = cleaned_isbn.chars().last();
+    let maybe_last = cleaned_isbn_cp.chars().last();
     let last = match maybe_last {
         Some(last) => last,
         None => {panic!("this is a terrible mistake!");},
@@ -20,7 +44,7 @@ fn main() {
         None => panic!("last letter \"{}\" is not a digit or \"X\"", last),
     };
 
-    let sliced_isbn = (&cleaned_isbn[..cleaned_isbn.len()-1]).to_string();
+    let sliced_isbn = (&cleaned_isbn_cp[..cleaned_isbn_cp.len()-1]).to_string();
     println!("sliced_isbn: {}", sliced_isbn);
 
     let mut result = 0;
